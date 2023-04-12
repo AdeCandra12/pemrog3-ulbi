933f54d1881df1e33511125265225d739b2c28d7
AdeCandra12 <ade.can321@gmail.com>
2023-04-12 09:50:24 +0700

    home

--- url/url.go
+++ url/url.go
@@ -11,7 +11,7 @@ func Web(page *fiber.App) {
 	page.Post("/api/whatsauth/request", controller.PostWhatsAuthRequest)  //API from user whatsapp message from iteung gowa
 	page.Get("/ws/whatsauth/qr", websocket.New(controller.WsWhatsAuthQR)) //websocket whatsauth
 
-	// page.Get("/", controller.Homepage) //ujicoba panggil package musik
+	page.Get("/", controller.Homepage) //ujicoba panggil package musik
 	// page.Get("/presensi", controller.GetPresensi)
 
 	page.Get("/presensi", controller.GetAllPresensi)    //menampilkan seluruh data presensi
